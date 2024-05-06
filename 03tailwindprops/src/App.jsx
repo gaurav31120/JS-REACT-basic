3d6db6913301ad2350417d35e1730e373aadaaa5
Gaurav Kumar <gaurav31120@gmail.com>
2024-05-06 07:52:10 +0530

    tailwind css and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  // let myObj = {
+  //   username: "gaurav",
+  //   age: 21,
+  // }
+
+  // let newArr = [1,2,3]
+
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>
+      Tailwind test
+    </h1>
+    <Card username="chaiaurcode" btnText="click me" />
+    <Card username="gaurav"  btnText="visit me"/>
+    
+
+    
+    </>
+  )
+}
+
+export default App
